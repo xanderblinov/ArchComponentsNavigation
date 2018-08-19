8015c2eb21cee805b5a8c56008e6e16874dd3d3c
a.blinov <a.blinov@hh.ru>
2018-08-19 12:25:26 +0300

    part1 jetifiering

--- NavSamplePart1/app/src/main/java/com/github/xanderblinov/nav/MainActivity.kt
+++ NavSamplePart1/app/src/main/java/com/github/xanderblinov/nav/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.github.xanderblinov.nav
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
--- NavSamplePart1/app/src/main/java/com/github/xanderblinov/nav/NotificationFragment.kt
+++ NavSamplePart1/app/src/main/java/com/github/xanderblinov/nav/NotificationFragment.kt
@@ -1,10 +1,10 @@
 package com.github.xanderblinov.nav
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_notification.*
 
